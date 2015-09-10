9699a46690eeb33be29dde47c5762e99e22b0516
julianschulte <julian.schulte-hullern@gmx.de>
2015-09-10 14:20:31 +0200

    Configurate for Windows and Mac

---
+++ MADX/src/de/schule/madx/server/WebSocketEndpoint.java
@@ -0,0 +1,39 @@
+package de.schule.madx.server;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.websocket.OnClose;
+import javax.websocket.OnError;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.server.ServerEndpoint;
+
+@ServerEndpoint("/serverendpoint")
+public class WebSocketEndpoint {
+	
+	Logger logger = Logger.getLogger(WebSocketEndpoint.class.getName());
+	
+	@OnOpen
+	public void handleOpen() {
+		logger.log(Level.INFO, "client connected...");
+	}
+	
+	@OnClose
+	public void handleClose() {
+		logger.log(Level.INFO, "client disconnected...");
+	}
+	
+	@OnMessage
+	public String handleMessage(String message) {
+		logger.log(Level.INFO, "client send:" + message);
+		return "echo" + message;
+	}
+	
+	@OnError
+	public void handleError(Throwable t) {
+		logger.log(Level.SEVERE, "Endpoint-Error",t);
+		t.printStackTrace();
+	}
+
+}
