3588c86b01607080f3d6537826ce77f8b8bb50d6
Chris Woelk <chris-wo@gmx.de>
2015-10-21 12:03:35 +0200

    Style Webcontent

--- MADNX/src/de/schule/madnx/client/view/MenueView.java
+++ MADNX/src/de/schule/madnx/client/view/MenueView.java
@@ -23,33 +23,51 @@ public class MenueView extends AbstractView implements Display{
 	private Button btnOptions;
 	private Button btnExit;
 
+	private FlowPanel panelBody;
+	private FlowPanel panel;
+	
+	private FlowPanel mainBox;
+
 	@Override
 	void init(FlowPanel rootPanel) {
 		lblTitle = new Label("Menü");
 		btnSinglePlayer = new Button("Einzelspieler");
-		btnMultiPlayer = new Button("Mehrspieler");
-		btnHighScore = new Button("HighScore");
-		btnOptions = new Button("Optionen");
-		btnExit = new Button("Beenden");
+		btnMultiPlayer = new Button("<i class='fa fa-users'></i> Mehrspieler");
+		btnHighScore = new Button("<i class='fa fa-list-ol'></i> HighScore");
+		btnOptions = new Button("<i class='fa fa-cog'></i> Optionen");
+		btnExit = new Button("<i class='fa fa-times-circle'></i> Beenden");
 		
-		rootPanel.add(lblTitle);
+		this.mainBox = new FlowPanel();
+		this.panel = new FlowPanel();
+		this.panelBody = new FlowPanel();
+		
+		this.panel.add(lblTitle);
 //		rootPanel.add(btnSinglePlayer);
-		rootPanel.add(btnMultiPlayer);
-		rootPanel.add(btnHighScore);
-		rootPanel.add(btnOptions);
-		rootPanel.add(btnExit);
+		this.panelBody.add(btnMultiPlayer);
+		this.panelBody.add(btnHighScore);
+		this.panelBody.add(btnOptions);
+		this.panelBody.add(btnExit);
+
+		this.panel.add(this.panelBody);
+		this.mainBox.add(this.panel);
+		rootPanel.add(this.mainBox);
 		
 	}
 
 	@Override
 	void initStyles() {
+		
+		this.mainBox.addStyleName("col-md-4 col-xs-10 col-md-offset-4 col-xs-offset-1");
+		
 		getRootPanel().addStyleName("menue-Main");
-		lblTitle.addStyleName("menue-Title");
-		btnSinglePlayer.addStyleName("gwt-Button-Menue");
-		btnMultiPlayer.addStyleName("gwt-Button-Menue");
-		btnHighScore.addStyleName("gwt-Button-Menue");
-		btnOptions.addStyleName("gwt-Button-Menue");
-		btnExit.addStyleName("gwt-Button-Menue");
+		this.panel.addStyleName("panel panel-default");
+		this.panelBody.addStyleName("panel-body");
+		lblTitle.addStyleName("menue-Title panel-heading");
+		btnSinglePlayer.addStyleName("gwt-Button-Menue btn btn-success btn-block");
+		btnMultiPlayer.addStyleName("gwt-Button-Menue btn btn-success btn-block");
+		btnHighScore.addStyleName("gwt-Button-Menue btn btn-primary btn-block");
+		btnOptions.addStyleName("gwt-Button-Menue btn btn-primary btn-block");
+		btnExit.addStyleName("gwt-Button-Menue btn btn-danger btn-block");
 	}
 
 	@Override
--- MADNX/src/de/schule/madnx/client/view/MultiPlayerMenueView.java
+++ MADNX/src/de/schule/madnx/client/view/MultiPlayerMenueView.java
@@ -13,27 +13,47 @@ public class MultiPlayerMenueView   extends AbstractView implements Display{
 	private Button btnConnect;
 	private Button btnCreate;
 	private Button btnClose;
+	
+	private FlowPanel panel;
+	private FlowPanel panelBody;
+	
+	private FlowPanel mainBox;
 
 	@Override
 	void init(FlowPanel rootPanel) {
 		lblTitle = new Label("Multiplayer Menü");
-		btnConnect = new Button("Beitreten");
-		btnCreate = new Button("Anlegen");
-		btnClose = new Button("zurück");
+		btnConnect = new Button("<i class='fa fa-users'></i> Beitreten");
+		btnCreate = new Button("<i class='fa fa-sign-in'></i> Anlegen");
+		btnClose = new Button("<i class='fa fa-chevron-circle-left'></i> zurück");
 		
-		rootPanel.add(lblTitle);
-		rootPanel.add(btnConnect);
-		rootPanel.add(btnCreate);
-		rootPanel.add(btnClose);
+		this.mainBox = new FlowPanel();
+		this.panel = new FlowPanel();
+		this.panelBody = new FlowPanel();
+		
+		this.panel.add(lblTitle);
+		this.panelBody.add(btnConnect);
+		this.panelBody.add(btnCreate);
+		this.panelBody.add(btnClose);
+		
+		this.panel.add(this.panelBody);
+
+		this.mainBox.add(this.panel);
+		rootPanel.add(this.mainBox);
 	}
 
 	@Override
 	void initStyles() {
+		
+		this.mainBox.addStyleName("col-md-4 col-xs-10 col-md-offset-4 col-xs-offset-1");
+		
+		this.panel.addStyleName("panel panel-default");
+		this.panelBody.addStyleName("panel-body");
+		
 		getRootPanel().addStyleName("menue-Main");
-		lblTitle.addStyleName("menue-Title");
-		btnConnect.addStyleName("gwt-Button-Menue");
-		btnCreate.addStyleName("gwt-Button-Menue");
-		btnClose.addStyleName("gwt-Button-Menue");
+		lblTitle.addStyleName("menue-Title panel-heading");
+		btnConnect.addStyleName("gwt-Button-Menue btn btn-primary btn-block");
+		btnCreate.addStyleName("gwt-Button-Menue btn btn-success btn-block");
+		btnClose.addStyleName("gwt-Button-Menue btn btn-link btn-block");
 	}
 
 	@Override
